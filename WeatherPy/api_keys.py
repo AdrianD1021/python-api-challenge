0b0d2ff10ddd68684b43a286a62589981f5edcae
AdrianD1021 <adriandd1021@gmail.com>
2023-12-12 18:43:40 -0500

    3rd Commit

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "45d1b9ab138d593a58a5b523c042085d"
 
 # Geoapify API Key
 geoapify_key = "05d978508bc94a089049577f08b2cdaa"
